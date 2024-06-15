41fcd2ebd6209be6682d737557ae17584c060b83
facurzn21 <facundoeze@gmail.com>
2024-06-15 14:11:41 -0300

    FIX

--- src/features/quote/Cita.test.tsx
+++ src/features/quote/Cita.test.tsx
@@ -3,6 +3,9 @@ import { server } from "../../mocks/server";
 import { render } from "../../test-utils";
 import Cita from "./Cita";
 import userEvent from "@testing-library/user-event";
+import { MENSAJE_CARGANDO, NOMBRE_INVALIDO, NO_ENCONTRADO } from "./constants";
+import { API_URL } from "../../app/constants";
+import { rest } from "msw";
 
 // servidor mock
 beforeAll(() => server.listen()); // Iniciar el servidor 
@@ -12,62 +15,77 @@ afterAll(() => server.close()); // se cierra el servidor despues de las pruebas
 describe("Componente Cita", () => {
   it("debería mostrar la cita por defecto", () => {
     render(<Cita />);
-    expect(screen.getByText("No se encontro ninguna cita")).toBeInTheDocument();
+    expect(screen.getByText(NO_ENCONTRADO)).toBeInTheDocument();
   });
 
   it("debería mostrar 'CARGANDO...' al hacer clic en el botón de cita aleatoria", async () => {
     render(<Cita />);
-    userEvent.click(screen.getByText("Obtener cita aleatoria")); // Hace click en el botón
-    expect(await screen.findByText("CARGANDO...")).toBeInTheDocument(); // Verifica que el texto de carga aparezca
+    userEvent.click(screen.getByText("Obtener cita aleatoria"));
+    expect(await screen.findByText(MENSAJE_CARGANDO)).toBeInTheDocument();
   });
 
   it("debería mostrar 'CARGANDO...' al hacer clic en el botón de cita de personaje", async () => {
     render(<Cita />);
     const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    userEvent.type(input, "Milhouse Van Houten"); // Escribe en el campo de entrada
-    userEvent.click(screen.getByText("Obtener Cita")); // Hace click en el botón
-    expect(await screen.findByText("CARGANDO...")).toBeInTheDocument();
+    userEvent.type(input, "Milhouse Van Houten");
+    userEvent.click(screen.getByText("Obtener Cita"));
+    expect(await screen.findByText(MENSAJE_CARGANDO)).toBeInTheDocument();
   });
 
   it("debería mostrar la cita del personaje al hacer clic en el botón con un valor válido", async () => {
+    server.use(
+      rest.get(`${API_URL}`, (_req, res, ctx) => {
+        return res(ctx.json([{ quote: "But my mom says I'm cool.", character: "Milhouse Van Houten" }]));
+      })
+    );
+    
     render(<Cita />);
     const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    userEvent.type(input, "Milhouse Van Houten"); // Escribe en el campo de entrada
-    userEvent.click(screen.getByText("Obtener Cita")); // Hace click en el botón
-    expect(
-      await screen.findByText("But my mom says I'm cool.") // Verifica la cita específica
-    ).toBeInTheDocument();
-    expect(screen.queryByText("Bart Simpson")).not.toBeInTheDocument();
+    userEvent.type(input, "Milhouse Van Houten");
+    userEvent.click(screen.getByText("Obtener Cita"));
+    expect(await screen.findByText("But my mom says I'm cool.")).toBeInTheDocument();
+    expect(screen.queryByText(NO_ENCONTRADO)).not.toBeInTheDocument();
   });
 
   it("debería mostrar una cita aleatoria al hacer clic en el botón sin valor de entrada", async () => {
+    server.use(
+      rest.get(`${API_URL}`, (_req, res, ctx) => {
+        return res(ctx.json([{ quote: "Thank you. Come again." }]));
+      })
+    );
+
     render(<Cita />);
-    userEvent.click(screen.getByText("Obtener cita aleatoria")); // Hace click en el botón
-    expect(
-      await screen.findByText("Thank you. Come again.")
-    ).toBeInTheDocument();
-    expect(
-      screen.queryByText("No se encontro ninguna cita")
-    ).not.toBeInTheDocument();
+    userEvent.click(screen.getByText("Obtener cita aleatoria"));
+    expect(await screen.findByText("Thank you. Come again.")).toBeInTheDocument();
+    expect(screen.queryByText(NO_ENCONTRADO)).not.toBeInTheDocument();
   });
 
   it("debería mostrar un error cuando no se encuentra el nombre del personaje", async () => {
+    server.use(
+      rest.get(`${API_URL}`, (_req, res, ctx) => {
+        return res(ctx.status(404), ctx.json({ error: "Por favor ingrese un nombre válido" }));
+      })
+    );
+
     render(<Cita />);
     const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    userEvent.type(input, "Facundo Correa"); // Escribe en el campo de entrada
-    userEvent.click(screen.getByText("Obtener Cita")); // Hace click en el botón
-    expect(
-      await screen.findByText("Por favor ingrese un nombre válido")
-    ).toBeInTheDocument();
+    userEvent.type(input, "Facundo Correa");
+    userEvent.click(screen.getByText("Obtener Cita"));
+    expect(await screen.findByText(NOMBRE_INVALIDO)).toBeInTheDocument();
   });
 
+  
   it("debería mostrar un error con una entrada inválida", async () => {
+    server.use(
+      rest.get(`${API_URL}`, (_req, res, ctx) => {
+        return res(ctx.status(404), ctx.json({ error: "Por favor ingrese un nombre válido" }));
+      })
+    );
+    
     render(<Cita />);
     const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    userEvent.type(input, "1"); // Escribe en el campo de entrada
-    userEvent.click(screen.getByText("Obtener Cita")); // Hace click en el botón
-    expect(
-      await screen.findByText("Por favor ingrese un nombre válido")
-    ).toBeInTheDocument();
+    userEvent.type(input, "1");
+    userEvent.click(screen.getByText("Obtener Cita"));
+    expect(await screen.findByText(NOMBRE_INVALIDO)).toBeInTheDocument();
   });
 });
